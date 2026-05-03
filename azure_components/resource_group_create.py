6e6d80eee8d75fa83f7bf3e75c797c5800b7d590
Moukouba <m_moukouba@yahoo.fr>
2026-05-03 06:19:47 +0200

    trial

--- azure_components/resource_group_create.py
+++ azure_components/resource_group_create.py
@@ -2,6 +2,7 @@ import argparse
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
 
+# this function is meant to create resource groupgit reset --hard devops/main ###
 def create_resource_group(subscription_id, resource_group_name, location, tags):
     """
     Create an Azure Resource Group using the Azure SDK for Python, with optional tags.
